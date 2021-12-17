1387f8468bc129336a76a0ec84f938c16b6b9b89
Castaneche <baptiste.odet1@gmail.com>
2021-12-17 16:03:58 +0100

    fix: compilation errors for fft header

--- src/Numa/FFT.h
+++ src/Numa/FFT.h
@@ -6,6 +6,8 @@
 namespace numa {
     namespace fft {
 
+		enum class Window { None, Hamming, Hann, Blackmann };
+
 		//Perform fft on data. 
 		//WARNING : input data will be modified
 		void spectrum(std::vector<double>& xdata, std::vector<double>& ydata, Window windowFunction = Window::None);
@@ -13,9 +15,6 @@ namespace numa {
 		//Window functions 
 		// https://matthieu-brucher.developpez.com/tutoriels/algo/fft/#LII-B
 		// https://en.wikipedia.org/wiki/Window_function#Blackman_window
-
-		enum class Window { None, Hamming, Hann, Blackmann };
-
 		void applyHann(std::vector<double>& arr);
 		void applyHamming(std::vector<double>& arr);
 		void applyBlackmann(std::vector<double>& arr);
